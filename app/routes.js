a05aa0a4d468a00a0a3525ef88caf8dca13dade5
Conor Keating <c.keating133@yahoo.ie>
2017-11-20 22:06:55 -0500

    adding song object through HTML form

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,22 @@
+var hello = require('../src/hello');
+var path = require('path');
+
 module.exports = function(app) {
 	//Show HomePage
 	app.get('/', function(req, res) {
-		res.send("hello lad");
+		var indexPath = path.resolve('views/index.html');
+		res.sendFile(indexPath)
+		//res.send("hello lad");
 	});	
 	
 	app.get('/twat', function(req, res) {
 		res.send("youre a twat");
+	});	
+	
+	app.get('/hello', function(req, res) {
+		res.send("helol");
+		hello.helloworld();
+
+		hello.helloworld2();
 	});	
 }
