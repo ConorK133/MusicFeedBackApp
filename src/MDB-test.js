dee95d56109421414bee431741273f914b6de851
Joseph Gillen <joegillen@ymail.com>
2017-12-06 02:44:38 +0000

    Setting up connection to database and insertion of object

--- src/MDB-test.js
+++ src/MDB-test.js
@@ -5,14 +5,15 @@ try{
     console.log("Didn't connect");
 }
 function createDatabase(){
-    socket.emit('database');
+    var song3 = {"title" : "Test Song 4", "artist" : "Tester", "genre" : "Rock", "Desc" : "Test song for testing", "audioId" : "4" };
+    socket.emit('database', song3);
 }
 
 window.onload = function(){
     var button = document.getElementById('database');
     if(button){
         button.addEventListener('click', function(){
-            createDatabase();  
+            createDatabase();
         })
     }
 }
--- src/MongoDB-init.js
+++ src/MongoDB-init.js
@@ -4,30 +4,48 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
-
-var song = { "title" : "My Test Song", "artist" : "Tester", "genre" : "Rock", "Desc" : "Test song for testing", "audioId" : "1" };
+// URL to our database.
+var MONGO_URL = "mongodb://MusicFeedBackApp:1677104@ds129906.mlab.com:29906/musicfeedbackapp";
 
 exports = module.exports = function(io){
     io.on('connection', function(socket){
-        socket.on('database', function(){
-            MongoClient.connect(url, function(err, db){
-                if(err) throw err;
-                console.log("Database created");
-                db.createCollection("songs", function(err, res){
-                    if(err) throw err;
-                    // Add to collection
-                    db.collection('songs').insert(song, function(err, res){
-                        if(err) throw err;
-                        console.log("Song inserted");
+        socket.on('database', function(song){
+            MongoClient.connect(MONGO_URL, function(err, db){
+                if(err){
+                    console.log("ERROR: " + err);
+                    throw err;
+                }
+                // Check if song exists in database by audioId.
+                function checkSongExists(song){
+                    // Query for findOne must be an object.
+                    db.collection('songs').findOne({audioId:song.audioId}, function(err, result){
+                        if(err){
+                            console.log("ERROR: " + err);
+                            throw err;
+                        }
+                        // Check if the collection contains any documents, if it does, continue...
+                        if(result != null){
+                            // then check if song you're attempting to add is already in the database.
+                            if(song.audioId != result.audioId){
+                                addSong(song);
+                            }
+                        }
+                        // If the collection has no documents then add the song.
+                        else if(result == null){
+                            addSong(song);
+                        }
                     });
-                    // Query collection
-                    db.collection('songs').findOne({}, function(err, res){
-                        if(err) throw err;
-                        console.log("Song returned: " + res.title, res.audioId, res.genre);
+                }
+                // Add to collection
+                function addSong(song){
+                    db.collection('songs').insert(song, function(err, res){
+                        if(err) {
+                            console.log("ERROR: " + err)
+                            throw err;
+                        }
                     });
                     db.close();
-                });
+                }
             });
         });
     });
